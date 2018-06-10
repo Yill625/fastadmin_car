be3c97ea1128c71d44ac76b59963364fda6bc275
亮亮 <1095036273@qq.com>
2018-06-10 17:59:18 +0800

    添加车号管理

---
+++ application/admin/lang/zh-cn/card.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'  =>  '序号',
+    'Card_number'  =>  'sim卡号',
+    'Status'  =>  '状态',
+    'Car_number'  =>  '车牌',
+    'Start_time'  =>  '开始时间',
+    'Service_year'  =>  '服务周期',
+    'Notice_time'  =>  '到期提醒'
+];
---
+++ public/assets/js/backend/card.js
@@ -0,0 +1,55 @@
+define(['jquery', 'bootstrap', 'backend', 'table', 'form'], function ($, undefined, Backend, Table, Form) {
+
+    var Controller = {
+        index: function () {
+            // 初始化表格参数配置
+            Table.api.init({
+                extend: {
+                    index_url: 'card/index',
+                    add_url: 'card/add',
+                    edit_url: 'card/edit',
+                    del_url: 'card/del',
+                    multi_url: 'card/multi',
+                    table: 'card',
+                }
+            });
+
+            var table = $("#table");
+
+            // 初始化表格
+            table.bootstrapTable({
+                url: $.fn.bootstrapTable.defaults.extend.index_url,
+                pk: 'id',
+                sortName: 'id',
+                columns: [
+                    [
+                        {checkbox: true},
+                        {field: 'id', title: __('Id')},
+                        {field: 'card_number', title: __('Card_number')},
+                        {field: 'status', title: __('Status'), formatter: Table.api.formatter.status},
+                        {field: 'car_number', title: __('Car_number')},
+                        {field: 'start_time', title: __('Start_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'service_year', title: __('Service_year')},
+                        {field: 'notice_time', title: __('Notice_time'), operate:'RANGE', addclass:'datetimerange', formatter: Table.api.formatter.datetime},
+                        {field: 'operate', title: __('Operate'), table: table, events: Table.api.events.operate, formatter: Table.api.formatter.operate}
+                    ]
+                ]
+            });
+
+            // 为表格绑定事件
+            Table.api.bindevent(table);
+        },
+        add: function () {
+            Controller.api.bindevent();
+        },
+        edit: function () {
+            Controller.api.bindevent();
+        },
+        api: {
+            bindevent: function () {
+                Form.api.bindevent($("form[role=form]"));
+            }
+        }
+    };
+    return Controller;
+});
